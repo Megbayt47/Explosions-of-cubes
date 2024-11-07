a5003f7dc44756b05609d40e92e0ca475830c37d
Megabyt47 <Megabyt_iceman-47@mail.ru>
2024-11-07 23:13:05 +0300

    Update Exploder.cs

--- Assets/Scripts/Exploder.cs
+++ Assets/Scripts/Exploder.cs
@@ -6,15 +6,12 @@ public class Exploder : MonoBehaviour
     [SerializeField] private float _explosionForce;
 
     private float _explosionRadius;
-    private int _divider = 2;
 
     public void BlowUp(List<Cube> cubes, Cube cube)
     {
         foreach (var newCube in cubes)
         {
-            float newCubeWidth = cube.transform.localScale.x;
-
-            _explosionRadius = newCubeWidth / _divider;
+            _explosionRadius = newCube.transform.localScale.x;
 
             newCube.Rigidbody.AddExplosionForce(_explosionForce, cube.transform.position, _explosionRadius);
         }
